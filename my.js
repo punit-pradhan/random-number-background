d712af260003e5bf032a228e3e6a2c74f3272971
Punit Pradhan <125362792+punit-pradhan@users.noreply.github.com>
2023-12-15 17:12:11 +0530

    Update my.js

--- my.js
+++ my.js
@@ -14,7 +14,7 @@ const handleOnMove = e => {
   letters.style.setProperty("--x", `${x}px`);
   letters.style.setProperty("--y", `${y}px`);
 
-  letters.innerText = randomString(1900);    
+  letters.innerText = randomString(3000);    
 }
 
 card.onmousemove = e => handleOnMove(e);
